c6132d5916d1f2ba38fe5521c11a5dc261eb304a
Tien <tientv.63.student@fit.tdc.edu.vn>
2022-04-15 08:03:08 +0700

    update

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -1,15 +1,14 @@
 <?php
-/**
-@ Chèn CSS và Javascript vào theme
-@ sử dụng hook wp_enqueue_scripts() để hiển thị nó ra ngoài front-end
-**/
-function my_styles() {
-/*
-* Hàm get_stylesheet_uri() sẽ trả về giá trị dẫn đến file style.css của theme
-* Nếu sử dụng child theme, thì file style.css này vẫn load ra từ theme mẹ
-*/
-wp_register_style( 'main-style', get_template_directory_uri() . '/style.css',
-'all' );
-wp_enqueue_style( 'main-style' );
+// Social
+if(!function_exists('shortcode_social')){
+    function shortcode_social($atts, $content){
+        $icon = $link = '';
+        $attr = shortcode_atts( array(
+            'icon' => 'fa-facebook',
+            'link' => '#',
+        ), $atts );
+
+        echo '<a href="'. $link .'"><i class="fa '.$icon.'"></i>'. $content .'</a>';
+    }
+    add_shortcode('social', 'shortcode_social');
 }
-add_action( 'wp_enqueue_scripts', 'my_styles' );
--- wp-content/themes/mytheme/index.php
+++ wp-content/themes/mytheme/index.php
@@ -1,4 +1,19 @@
 <?php
 get_header();
-   
+$args = array(
+   'post_type'=>'post', //Loại là post
+   'orderby' => 'rand', //Lấy ngẫu nhiên
+   'posts_per_page' => 5 //Lấy 5 bài post 1 trang
+);
+$the_query = new WP_Query( $args );
 ?>
+<div class="container mt-5">
+   
+<?php
+if ($the_query->have_posts()):
+   while ($the_query->have_posts()):
+       $the_query->the_post();
+       echo the_title("<br>");
+   endwhile;endif;
+?>
+</div>
